5e4e43c1490cadf0ebae69f9dced56ed2bea7f63
Babu Nair <babunairofficial@gmail.com>
2025-10-11 08:48:21 +0530

    Delete model - deleteOne

--- mongo1/index.js
+++ mongo1/index.js
@@ -23,10 +23,7 @@ const userSchema = new mongoose.Schema({
 //models also named as the collection, User
 const User = mongoose.model("User", userSchema);
 
-//Update model - findByIdAndUpdate
-User.findByIdAndUpdate("68e9178b98d19c1a03b2dd25", {name: "laxman"}, {new: true}).then((res) => { //update one document having having a ID
-  //and display the post-update data as options object have been used. 
+//DELETE model - deleteOne
+User.deleteOne({name: "ravi"}).then((res) => {  //delete one document 
   console.log(res);
-}).catch((err) => {
-  console.log(err);
 });
